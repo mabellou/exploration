87380d177ed17c0ac39ff2dde3d13e60f369f2b9
mabellou <ma@mac.be>
2018-04-01 20:42:21 +0200

    Update files

--- Java/spring-5-web-functional/src/main/java/com/mabellou/web/functional/Application.java
+++ Java/spring-5-web-functional/src/main/java/com/mabellou/web/functional/Application.java
@@ -1,12 +1,12 @@
-package com.mabellou.spring.flux.client;
+package com.mabellou.web.functional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringFlux1Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringFlux1Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
