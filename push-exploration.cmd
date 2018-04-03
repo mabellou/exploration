4ea7a96afa6b4541ad4940b27b2589fdce5e31f8
mabellou <ma@mac.be>
2018-04-03 19:51:21 +0200

    Automatically commited

---
+++ push-exploration.cmd
@@ -0,0 +1,4 @@
+cd "C:\Users\Mac\Code\exploration"
+"C:\Program Files\Git\bin\git.exe" add .
+"C:\Program Files\Git\bin\git.exe" commit -m "Automatically commited"
+"C:\Program Files\Git\bin\git.exe" push
