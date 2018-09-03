4cc13f4d0d63d3c275d40cbddfb718ab574d3a06
mac <mab@mab.com>
2018-09-03 08:46:56 +0200

    Add tests for complex specification

--- Pattern/specification-pattern/src/test/java/com/mabellou/specification/composite/SpecificationTest.java
+++ Pattern/specification-pattern/src/test/java/com/mabellou/specification/composite/SpecificationTest.java
@@ -121,4 +121,60 @@ public class SpecificationTest {
 		boolean result = negationSpecification.isSatisfiedBy(refrigeratedContainer);
 		assertFalse(result);
 	}
+
+	@Test
+	public void and_Or_Negate_Specification_True(){
+		Specification isTrue1 =
+				new EqualSpecification(-20, Container::getTemperatureMax);
+		Specification isTrue2 =
+				new EqualSpecification(-30, Container::getTemperatureMin);
+		Specification isFalse1 =
+				new EqualSpecification(-21, Container::getTemperatureMax);
+		Specification isFalse2 =
+				new EqualSpecification(-31, Container::getTemperatureMin);
+
+		Specification andSpecification =        isFalse1.or(isFalse2)
+										.and(
+												isTrue1.or(isTrue2)
+										).negate();
+
+		boolean result = andSpecification.isSatisfiedBy(refrigeratedContainer);
+		assertTrue(result);
+
+		Specification orSpecification =        isFalse1.and(isTrue1)
+										.or(
+												isFalse2.and(isTrue2)
+										).negate();
+
+		result = orSpecification.isSatisfiedBy(refrigeratedContainer);
+		assertTrue(result);
+	}
+
+	@Test
+	public void and_Or_Negate_Specification_False(){
+		Specification isTrue1 =
+				new EqualSpecification(-20, Container::getTemperatureMax);
+		Specification isTrue2 =
+				new EqualSpecification(-30, Container::getTemperatureMin);
+		Specification isFalse1 =
+				new EqualSpecification(-21, Container::getTemperatureMax);
+		Specification isFalse2 =
+				new EqualSpecification(-31, Container::getTemperatureMin);
+
+		Specification andSpecification =        isFalse1.or(isTrue1)
+										.and(
+												isTrue2.or(isFalse2)
+										).negate();
+
+		boolean result = andSpecification.isSatisfiedBy(refrigeratedContainer);
+		assertFalse(result);
+
+		Specification orSpecification =        isFalse1.and(isFalse2)
+										.or(
+												isTrue1.and(isTrue2)
+										).negate();
+
+		result = orSpecification.isSatisfiedBy(refrigeratedContainer);
+		assertFalse(result);
+	}
 }
